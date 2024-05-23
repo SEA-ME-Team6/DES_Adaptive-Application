e1cca1cecc67ceae197efdac0c3185e600ee413c
K0Dahyun <gdh1685@kookmin.ac.kr>
2024-05-23 18:26:13 +0200

    Imple findservice function

--- client_autosar_adaptive/ara/com/service_factory.cpp
+++ client_autosar_adaptive/ara/com/service_factory.cpp
@@ -6,31 +6,38 @@ namespace ara
     {
 
         ara::core::Result<ara::com::ServiceHandleContainer<HandleType>> ServiceFactory::FindService(ara::com::InstanceIdentifier& instance) {
-            // auto& client = ara::com::vsomeip_client::get_instance();
-                    
-        }
-
-        ara::core::Result<ara::com::FindServiceHandle> ServiceFactory::StartFindService(ara::com::FindServiceHandler<HandleType> handler, ara::com::InstanceIdentifier instance) {
             vsomeip_client sd_client = ara::com::vsomeip_client::get_client();
             sd_client.Init(instance);
             sd_client.set_service_id(instance);
+            
+            ServiceHandleContainer<HandleType> container;
+
             sd_client.register_availability_observer([&](bool is_available) {
                 if (is_available) {
-                    ServiceHandleContainer<HandleType> container;
 
                     ara::com::ServiceHandleType service_handle(instance);
                     container.push_back(service_handle);
 
                     ara::com::FindServiceHandle handle(instance.GetInstanceId(), instance.GetInstanceId());
-                    handler(container, handle);
                 }
             });
             sd_client.register_availability_handler();
             sd_client.start();
 
-            ara::com::FindServiceHandle success_handle(instance.GetInstanceId(), instance.GetInstanceId());
-            return ara::core::Result<ara::com::FindServiceHandle>(success_handle);
-        }   
+            return container;
+        }
+
+        ara::core::Result<ara::com::FindServiceHandle> ServiceFactory::StartFindService(ara::com::FindServiceHandler<HandleType> handler, ara::com::InstanceIdentifier instance) {
+            while (true) {
+                ara::core::Result<ara::com::ServiceHandleContainer<HandleType>> result = FindService(instance);
+
+                if (result.HasValue()) {
+                    handler(result.Value(), ara::com::FindServiceHandle(instance.GetInstanceId(), instance.GetInstanceId()));
+                }
+            }
+
+            std::this_thread::sleep_for(std::chrono::seconds(1));
+        }
 
     } // namespace com
 } // namespace ara
--- client_autosar_adaptive/ara/com/service_factory.h
+++ client_autosar_adaptive/ara/com/service_factory.h
@@ -19,7 +19,6 @@ namespace ara
 
             static ara::core::Result<ara::com::ServiceHandleContainer<HandleType>> FindService(ara::com::InstanceIdentifier& instance);
             static ara::core::Result<ara::com::FindServiceHandle> StartFindService(ara::com::FindServiceHandler<HandleType> handler, ara::com::InstanceIdentifier instance);
-
         };
     } // namespace com
 } // namespace ara
